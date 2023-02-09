1407067a42b57737af474e09a16f2439349cad3e
MaxHowksbill <zabaramax3@gmail.com>
2023-02-09 19:51:50 +0200

    changes

--- Statistics/mode and mean.py
+++ Statistics/mode and mean.py
@@ -3,9 +3,9 @@ import statistics
 def Enter_array(arr):
     lst = list(arr.split(' '))
 
-    for i in range(0, len(lst)):
-        lst[i] = int(lst[i])
-
+    #for i in range(0, len(lst)):
+     #   lst[i] = int(lst[i])
+    lst = [int(lst[i]) for i in range(0, len(lst))]
     if statistics.mode(lst) != statistics.mean(lst):
         r = 0
     else:
